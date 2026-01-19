1cd04c7be651871e47291cbce7f67b8febe31de5
Hezjz <1204914@student.roc-nijmegen.nl>
2026-01-19 16:00:18 +0100

    I have done the hamburger menu.

--- javascript/general.js
+++ javascript/general.js
@@ -1,15 +1,18 @@
-sdocument.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-        hideMenu();
-    }
-});
+const navigation = document.getElementById("nav-mobile");
+navigation.style.display = "none";
 
 
-function togglenav() {
-    const navigation = document.getElementById("nav-mobile")
+function toggleNav() {
      if (navigation.style.display == "none") {
          navigation.style.display = "flex";
      } else {
          navigation.style.display = "none";
     }
 }
+
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        hideMenu();
+    }
+});
